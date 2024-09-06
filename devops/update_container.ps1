545163b6745c4fa8ae391061e1678ea73c5f4b16
Ryan Schofield <rschofield@eidebailly.com>
2024-09-06 09:46:23 -0600

    remove unnecessary steps

--- devops/update_container.ps1
+++
@@ -1,7 +0,0 @@
-Write-Output "Updating container"
-
-$folderPath = Get-Location | Select-Object | ForEach-Object{$_.ProviderPath}
-$filePath = Join-Path $folderPath -ChildPath "devops" | Join-Path -ChildPath "container_app_config.yml"
-Write-Output "Deploying from: $filePath"
-
-az container create -g InternalRG-Testing -f $filePath
